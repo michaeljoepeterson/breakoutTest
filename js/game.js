ea4e9c267256e3be6b3c83ac09ca986e6d02b089
Michael Peterson <michaeljoepeterson@gmail.com>
2020-10-10 15:59:03 -0600

    first moving ball

--- js/game.js
+++ js/game.js
@@ -2,6 +2,13 @@ class Game{
     canvas;
     canvasId;
     canvas2d;
+    renderInterval;
+    canvasHeight;
+    canvasWidth;
+    ballx = 5;
+    bally = 145;
+    aX = 2;
+    aY = -2;
 
     constructor(options){
         try{
@@ -9,35 +16,37 @@ class Game{
             this.canvasId = canvasId;
             this.canvas = document.getElementById(this.canvasId);
             this.canvas2d = this.canvas.getContext('2d');
-            this.render();
+            this.init();
         }
         catch(e){
             console.log('error initializing game: ', e);
         }
     }
 
-    render(){
-        //start path
-        this.canvas2d.beginPath();
-        //shape
-        this.canvas2d.rect(20, 40, 50, 50);
-        //color
-        this.canvas2d.fillStyle = "#FF0000";
-        //fill with color
-        this.canvas2d.fill();
-        //attempt to draw line to close
-        this.canvas2d.closePath();
+    init = () =>{
+        this.setResizeListener();
+        this.renderInterval = setInterval(this.gameRender,20);
+    }
 
+    gameRender = () =>{
+        this.renderBall();
+        this.ballx += this.aX;
+        this.bally += this.aY;
+    }
+
+    renderBall = (x,y) => {
+        x = x ? x : this.ballx;
+        y = y ? y : this.bally;
+        this.canvas2d.clearRect(0,0,this.canvasWidth,this.canvasHeight);
         this.canvas2d.beginPath();
-        this.canvas2d.arc(240, 100, 20, 0, Math.PI*2, false);
-        this.canvas2d.fillStyle = "green";
+        this.canvas2d.arc(x, y, 5, 0, Math.PI*2);
+        this.canvas2d.fillStyle = "#0095DD";
         this.canvas2d.fill();
         this.canvas2d.closePath();
+    }
 
-        this.canvas2d.beginPath();
-        this.canvas2d.rect(160, 10, 100, 40);
-        this.canvas2d.strokeStyle = "rgba(0, 0, 255, 0.5)";
-        this.canvas2d.stroke();
-        this.canvas2d.closePath();
+    setResizeListener = () =>{
+        this.canvasHeight = this.canvas.clientHeight;
+        this.canvasWidth = this.canvas.clientWidth;
     }
 }
